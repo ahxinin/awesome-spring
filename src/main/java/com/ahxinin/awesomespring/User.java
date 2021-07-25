e15cd976bb62a6d0b6d0b40433dabe43b758a8ec
hexin <ahxin1989@gmail.com>
2021-07-25 10:21:16 +0800

    首次提交

---
+++ src/main/java/com/ahxinin/awesomespring/User.java
@@ -0,0 +1,29 @@
+package com.ahxinin.awesomespring;
+
+/**
+ * @author : hexin
+ * @description: 用户
+ * @date : 2021-07-25
+ */
+public class User {
+
+    private String name;
+
+    private String blog;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBlog() {
+        return blog;
+    }
+
+    public void setBlog(String blog) {
+        this.blog = blog;
+    }
+}
